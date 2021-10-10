841f74aa7e53a3038b2d60668f33f17e221d18d2
Chip <login721@gmail.com>
2021-10-10 15:41:04 +0900

    fixed installer script

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ sudo apt-get install net-tools python3 python3-pip python3-venv
 python3 -m venv .venv
 .venv/bin/pip install -r requirements.txt
 
-yes | sudo cp -rf klipper-lunar-penguin.service /etc/systemd/system/test.service
+yes | sudo cp -rf klipper-lunar-penguin.service /etc/systemd/system/klipper-lunar-penguin.service
 
 sudo systemctl daemon-reload
 sudo systemctl start klipper-lunar-penguin
